b09c1b1da78f69ce7bbed1ae83c7b051b1e3fc7c
priya-ak <entraiotsolutions@gmail.com>
2026-08-25 00:13:34 +0530

    Remove accidentally duplicated nested project folder

--- Zivira-fieldRepo-main/app/field/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { FieldShell } from "@/components/field-shell";
-
-export default function FieldLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <FieldShell>{children}</FieldShell>;
-}
